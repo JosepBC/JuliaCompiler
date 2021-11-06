e80679c00c2d7801007a9797b71a001c236f2d22
Josep Bello Curto <bellocontacto@gmail.com>
2021-11-06 20:57:20 +0100

    Added flag to compile with math.h

--- compilar.sh
+++ compilar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 bison -t -d -v sintactico.y
 flex lexico.l
-gcc lex.yy.c sintactico.tab.c symtab.c types.c -lfl -g -o julia_compiler
+gcc lex.yy.c sintactico.tab.c symtab.c types.c -lfl -lm -g -o julia_compiler
