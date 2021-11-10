3d55575277bc534356a9b89b6ec1e8419b89968d
Josep Bello <bellocontacto@gmail.com>
2021-11-10 18:16:20 +0100

    Added flags to bison

--- compilar.sh
+++ compilar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-bison -t -d -v sintactico.y
+bison -t -d -v -r all -Wall -Werror sintactico.y
 flex lexico.l
 gcc lex.yy.c sintactico.tab.c symtab.c types.c arithmetic_ops.c bool_ops.c -lfl -lm -g -o julia_compiler
