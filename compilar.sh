9519c7eed231a47f981009e33403ad942634ce65
Josep Bello <bellocontacto@gmail.com>
2021-12-03 17:27:00 +0100

    Removed unnecesary compile file

--- compilar.sh
+++ compilar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 bison -t -d -v -r all -Wall -Werror sintactico.y
 flex lexico.l
-gcc lex.yy.c sintactico.tab.c symtab.c types.c arithmetic_ops.c bool_ops.c code_generation.c -lfl -lm -g -o julia_compiler
+gcc lex.yy.c sintactico.tab.c symtab.c types.c arithmetic_ops.c code_generation.c -lfl -lm -g -o julia_compiler
