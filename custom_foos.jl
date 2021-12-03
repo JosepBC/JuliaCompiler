3139b135cf013ffbf661e53b49b236514ef939ff
Josep Bello <bellocontacto@gmail.com>
2021-12-03 17:28:27 +0100

    Changed var name of matrix in function headers

--- custom_foos.jl
+++ custom_foos.jl
@@ -1,5 +1,5 @@
 function PUTVI(v::Vector{Int32}, size::Int32)
 function PUTVF(v::Vector{Float32}, size::Int32)
 
-function PUTMI(v::Matrix{Int32}, rows::Int32, cols::Int32)
-function PUTMF(v::Matrix{Float32}, rows::Int32, cols::Int32)
+function PUTMI(m::Matrix{Int32}, rows::Int32, cols::Int32)
+function PUTMF(m::Matrix{Float32}, rows::Int32, cols::Int32)
