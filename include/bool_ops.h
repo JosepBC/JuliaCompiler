bd23f83a1509cf8a8df17e1cc7bbb36979dbd11d
Josep Bello <bellocontacto@gmail.com>
2022-01-12 22:19:49 +0100

    Removed emet of bool ops

--- include/bool_ops.h
+++ include/bool_ops.h
@@ -1,6 +1,7 @@
 #ifndef BOOL_OPS_HEADER
 #define BOOL_OPS_HEADER
 #include "types.h"
+#include "code_generation.h"
 
 extern void do_bool_equals(Variable a, Variable b, Variable *dst);
 extern void do_bool_and(Variable a, Variable b, Variable *dst);
@@ -13,13 +14,4 @@ extern void do_bool_higher_equal(Variable a, Variable b, Variable *dst);
 extern void do_bool_lower_equal(Variable a, Variable b, Variable *dst);
 
 
-extern void emet_bool_equals(Variable a, Variable b, Variable *dst);
-extern void emet_bool_and(Variable a, Variable b, Variable *dst);
-extern void emet_bool_or(Variable a, Variable b, Variable *dst);
-extern void emet_bool_not(Variable a, Variable *dst);
-extern void emet_bool_diff(Variable a, Variable b, Variable *dst);
-extern void emet_bool_higher_than(Variable a, Variable b, Variable *dst);
-extern void emet_bool_lower_than(Variable a, Variable b, Variable *dst);
-extern void emet_bool_higher_equal(Variable a, Variable b, Variable *dst);
-extern void emet_bool_lower_equal(Variable a, Variable b, Variable *dst);
 #endif
