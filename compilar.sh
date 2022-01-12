f1d4938459849640721630b81c13bd332ed799a2
Josep Bello <bellocontacto@gmail.com>
2022-01-12 11:56:57 +0100

    Checks for build dir existance

--- compilar.sh
+++ compilar.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if [ ! -d "build" ]
+then
+    mkdir build
+fi
+
 bison -t -d -v -r all -Wall -Werror src/sintactico.y -o build/sintactico.tab.c
 flex -o build/lex.yy.c src/lexico.l
 gcc build/lex.yy.c  build/sintactico.tab.c src/symtab.c src/types.c src/arithmetic_ops.c src/code_generation.c -I include/ -lfl -lm -g -o julia_compiler
