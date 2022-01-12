5685f2b30e708636c82db4bf4bae090cce56c8a8
Josep Bello <bellocontacto@gmail.com>
2022-01-12 22:19:11 +0100

    Added new example program

--- in.jl
+++ in.jl
@@ -1,15 +1,6 @@
-function swap(v::Vector{Int32})
-    temp = v[0]
-    v[0] = v[1]
-    v[1] = temp
+i = 1
+total = (1.0 + 2 * 3) - i
+if total > 666.6 && total != 500.0
+    i = i + 1
 end
-
-function mean(a::Float32, b::Float32)::Float32
-    return (a + b) / 2
-end
-
-v = [1; 2; 3; 4]
-swap(v)
-x = 10.0 + 7.0
-i = 0
-z = x + mean(x - 7, v[3 + i] * 3.5)
+i
