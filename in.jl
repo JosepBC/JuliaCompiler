8432b1a2ba14e4041323526d17e533013b6172b9
Josep Bello <bellocontacto@gmail.com>
2022-01-12 22:52:54 +0100

    Added more complex example

--- in.jl
+++ in.jl
@@ -2,7 +2,14 @@ i = 1
 total = (1.0 + 2 * 3) - i
 if total > 666.6 && total != 500.0
     i = i + 1
+    if total < 10
+        i = i + 2
+    end
 else
-    i = i - 1
+    if total < 10
+        i = i - 1
+    else
+        i = i - 2
+    end
 end
 i
