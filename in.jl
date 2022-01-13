19fb5635c05801fbb9ab2792d42694d69604e7c7
Josep Bello <bellocontacto@gmail.com>
2022-01-13 12:34:04 +0100

    While loop example

--- in.jl
+++ in.jl
@@ -1,38 +1,14 @@
-function foo(i::Int32)::Int32
-    if i > 10
-        i = i + 1
-    else
-        i = i - 1
-    end
-    return i
-end
-
-function foo2(i::Int32)
-    if i > 10
-        i
-    else
-        i = i + 1
-        i
-    end
-end
-
-i = 1
-a = i + foo(i)
-a = foo(i)
-foo2(a)
-total = (1.0 + 2 * 3) - i
-if total > 666.6 && total != 500.0
-    i = i + 1
-    if total < 10
-        i = i + 2
-    end
-else
-    if total < 10
-        i = i - 1
-    else
-        i = i - 2
-    end
-end
-i
-
-
+i = 1 
+total = (1.0 + 2 * 3) - i 
+while total < 1000.0 do
+    if i * 2 == 1 
+        total = total * 2 
+    else 
+        total = total + 1 
+    end 
+    j = -i 
+    if total > 666.6 || total == 500.0 
+        i = i + 1 
+    end 
+end 
+total
