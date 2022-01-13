4ec27c2323ae3a716fd564f51b54b74cb8e9800d
Josep Bello <bellocontacto@gmail.com>
2022-01-13 10:59:31 +0100

    Added functions

--- in.jl
+++ in.jl
@@ -1,4 +1,23 @@
+function foo(i::Int32)::Int32
+    if i > 10
+        i = i + 1
+    else
+        i = i - 1
+    end
+    return i
+end
+
+function foo2(i::Int32)
+    if i > 10
+        i
+    else
+        i = i + 1
+        i
+    end
+end
+
 i = 1
+a = i + foo(i)
 total = (1.0 + 2 * 3) - i
 if total > 666.6 && total != 500.0
     i = i + 1
@@ -13,3 +32,5 @@ else
     end
 end
 i
+
+
